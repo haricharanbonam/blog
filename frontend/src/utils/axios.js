69a3353efebed08300b4ca8a0195d0214af04041
Hari Charan <haricharanbonam@gmail.com>
2025-11-23 17:24:05 +0530

    fixed some url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://blog-n2cx.onrender.com/",
   withCredentials: true, // Send cookies like refresh token
 });
 
