d627a81c010504fbc272d14278e0e374e9629e51
Bonam Hari Charan <haricharanbonam@gmail.com>
2025-11-24 17:50:04 +0530

    fixed the cookie issue for deployment

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -2,7 +2,7 @@ export const DB_NAME = "haricharan";
 
 export const options = {
   httpOnly: true,
-  secure: false, 
-  sameSite: "strict", 
+  secure: true, 
+  sameSite: "none", 
   path: "/",
 };
