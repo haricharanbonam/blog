3bad01ec431e153024c7fa7913360c5eff3b135e
Hari Charan <haricharanbonam@gmail.com>
2025-12-19 15:54:19 +0530

    haha

---
+++ frontend/public/pika.js
@@ -0,0 +1,8 @@
+console.log("just to check whether u are working or not");
+
+// this particular file can be only visible in prod cuz prod builds frontend instead of serving each file . 
+
+
+// they are placed in "dist"  contains decides that all are related to the Index.jsx and app.jsx (i dont know how it will identify)
+
+// it does nt care about files kept parallel to index.html and other files thtw wre not part of they dont 
