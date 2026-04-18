06b6e4d144d07677219f87c173f462e7b9d08d30
Hari Charan <haricharanbonam@gmail.com>
2026-04-18 11:34:30 +0530

    some additiosn

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ import { verifyJWT } from "./middlewares/auth.middleware.js";
 
 const app = express();
 
+// app.set("trust proxy", true);
 // app.use() //used for middleware and conifgurations
 // app.use(
 //   cors({
@@ -19,25 +20,23 @@ const app = express();
 //   }),
 // );
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173",
-//       "https://blog-alpha-smoky-31.vercel.app",
-//     ],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://blog-alpha-smoky-31.vercel.app"],
+    credentials: true,
+  }),
+);
 
-app.use(cors());
+// app.use(cors());
 
 app.use(
   express.json({
     limit: "16kb",
-  })
+  }),
 );
 
 app.use("/", (req, res, next) => {
+  const url = req.url;
   console.log(req.method + req.url);
   next();
 });
@@ -46,7 +45,7 @@ app.use(
   express.urlencoded({
     extended: true,
     limit: "16kb",
-  })
+  }),
 );
 // const update=async()=>
 // {
@@ -63,11 +62,10 @@ app.use(
 app.use(express.static("public"));
 
 app.use(cookieParser());
-app.get("/",(req,res)=>
-{
+app.get("/", (req, res) => {
   console.log(`the req came from ip ${req.ip}`);
-  return res.end("Server is running")
-})
+  return res.end("Server is running");
+});
 
 app.use("/user", userRouter);
 
@@ -84,6 +82,5 @@ app.use((err, req, res, next) => {
     errors: err.errors || [],
   });
 });
-
 
 export { app };
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ import connectDB from "./db/Connection.js";
 import dotenv from "dotenv";
 import express from "express";
 import { app } from "./app.js";
+import dns from 'node:dns';
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 dotenv.config({
   path: "./.env",
 });
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+  app.listen(process.env.PORT || 8000, () => {
       console.log("server is running");
     });
   })
